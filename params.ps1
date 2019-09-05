8fce8bcb85fb49876cdf0741a5a1bc641f3ba2a0
michaeln <nau.michael@googlemail.com>
2019-09-05 17:37:40 +0200

    splatting

--- params.ps1
+++ params.ps1
@@ -3,46 +3,59 @@ param(
     [Parameter(Mandatory = $true)]
     [String]
     $InstanceName,
-    
     [Parameter(Mandatory = $true)]
     [String]
     $Table,
-
     [Parameter()]
     [String]
     $Query,
-
     [Parameter()]
     [string[]]
     $Fields,
-
     [Parameter()]
     [int32]
     $Limit
 )
 
 begin {
+
+    $UriScheme = "https://"
+    $UriPath = "/api/now/table/" + $Table
+    $UriQuery = ""
+    $UriFields = "?sysparm_fields=" + [System.Web.HttpUtility]::UrlEncode($Fields -join ',')
+    
+    if ($Limit){
+        $UriLimit = "&sysparm_limit=" + $Limit
+    } else {
+        $UriLimit = ""    
+    }
+    
+
     $Uri = (
-        "https://" + 
+        $UriScheme + 
         $InstanceName +
-        ".service-now.com/api/now/table/" +
-        $Table +
-        "?sysparm_fields=" +
-        [System.Web.HttpUtility]::UrlEncode($Fields -join ',') +
-        "&sysparm_limit=" +
-        $Limit + 
+        ".service-now.com" + 
+        $UriPath +
+        $UriFields + 
+        $UriLimit +
         "&sysparm_display_value=true"
     )
+
+    $Args = @{
+        Method = "Get"
+        Uri = $Uri
+        Credential = Get-Credential
+        Proxy = "http://proxy.example.com:8000"
+        ProxyUseDefaultCredentials = $true
+    }
+
 }
 
 process {
 
-    #$Fields = ('one', 'two', 'three')
-    #$Fields | Get-Member
-    #[System.Web.HttpUtility]::UrlEncode($Fields)
-
-    $Response = Invoke-WebRequest -Method Get -Uri $Uri -Credential $Cred -Proxy "http://clientproxy.corp.int:8080" -ProxyUseDefaultCredentials
+    $Response = Invoke-WebRequest @Args
     $Response.Content | ConvertFrom-Json | Select-Object -ExpandProperty result | Sort-Object -Property number
+
 }
     
 end {

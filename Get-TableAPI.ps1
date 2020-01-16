d71413591e24c7434d7571a80081f958c0d00d8c
Michael <36858344+jampattern@users.noreply.github.com>
2020-01-17 00:33:29 +0100

    Load configuration from JSON, work in progress

--- Get-TableAPI.ps1
+++ Get-TableAPI.ps1
@@ -3,18 +3,23 @@ param(
     [Parameter(Mandatory = $true)]
     [String]
     $Instance,
+
     [Parameter(Mandatory = $true)]
     [String]
     $Table,
+
     [Parameter()]
     [String]
     $Query,
+
     [Parameter()]
     [string[]]
     $Fields,
+
     [Parameter()]
     [int32]
     $Limit,
+
     [Parameter()]
     [String]
     $CrdFilePath,
@@ -23,46 +28,72 @@ param(
     [Parameter()]
     [string]
     $Proxy,
+
     [Parameter()]
     [switch]
-    $ProxyUseDefaultCrd
+    $ProxyUseDefaultCrd,
+
+    # configuration
+    [ArgumentCompleter( {
+            param ( $commandName,
+                $parameterName,
+                $wordToComplete,
+                $commandAst,
+                $fakeBoundParameters )
+            $possibleValues = Get-Content -Path .\configs.json | ConvertFrom-Json
+            $possibleValues.Configurations | ForEach-Object { $_.Name }
+        })]
+    [String] $Configuration
 )
 
 begin {
-
-    if (Test-Path $CrdFilePath){
+    <# **************************
+    import or ask for credentials
+    ************************** #>
+    if (Test-Path $CrdFilePath) {
         $Cred = Import-Clixml -Path $CrdFilePath
-    } else {
+    }
+    else {
         $Cred = Get-Credential
     }
 
+    
 
+    <# ******
+    build uri
+    ****** #>
     $UriBase = "https://" + $Instance + ".service-now.com/api/now"
     $UriResource = "/table/" + $Table
 
     $UriQuery = @()
-
-    if ($Fields){
+    if ($Fields) {
         $UriQuery += "sysparm_fields=" + [System.Web.HttpUtility]::UrlEncode($Fields)
     }
 
-    if ($Query){
+    if ($Query) {
         $UriQuery += "sysparm_query=" + [System.Web.HttpUtility]::UrlEncode($Query)
     }
 
-    if ($Limit){
+    if ($Limit) {
         $UriQuery += "sysparm_limit", $Limit -join "="
     }
 
     $UriQueryEnc = "?" + ($UriQuery -join "&")
     $Uri = $UriBase + $UriResource + $UriQueryEnc
 
+
+    <# ***********
+    request config
+    *********** #>
     $Conf = @{
         Method     = "Get"
         Uri        = $Uri
         Credential = $Cred
     }
 
+    <# **********
+    proxy options
+    ********** #>
     if ($Proxy) {
         $Conf.Proxy = $Proxy
         if ($ProxyUseDefaultCrd) {
@@ -72,9 +103,23 @@ begin {
 }
 
 process {
-    $Response = Invoke-WebRequest @Conf
-    $Response.Content | ConvertFrom-Json | Select-Object -ExpandProperty result
+    
+    try {
+        $Response = Invoke-WebRequest @Conf
+        $Result = $Response.Content | ConvertFrom-Json | Select-Object -ExpandProperty result
+        $Result    
+    }
+    catch {
+
+        $LogMessage = 
+        ("Error:",
+            $Error.ErrorRecord.CategoryInfo.Reason -join " "),
+            $Error.ErrorRecord.CategoryInfo.Activity -join ";"
+        
+        $LogMessage
+    }
 }
+    
     
 end {
 }
